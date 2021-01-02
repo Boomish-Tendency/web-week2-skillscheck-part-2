a5b4f1d7a83f7de7e9eaa8c582d455a831611c68
Brian Lindeboom <bklindeboom@gmail.com>
2021-01-02 17:19:59 -0500

    constructor

--- js-competencies/constructors-functions/constructors-functions.js
+++ js-competencies/constructors-functions/constructors-functions.js
@@ -6,18 +6,10 @@
 //BROKEN ON PURPOSE 
 
 function EgyptianGoddesses(name, oversees, strength, weakness) {
-  name = name;
-  oversees = oversees;
-  strength = strength;
-  weakness = weakness;
-
-  return function() {
-    name,
-    oversees,
-    strength,
-    weakness
-  }
-  
+  this.name = name;
+  this.oversees = oversees;
+  this.strength = strength;
+  this.weakness = weakness; 
 }
 
 // Use the variable maatCharacteristics below to create a new Egyptian Goddess - save it to 
@@ -28,6 +20,7 @@ var maatCharacteristics = ['Maat', 'Truth and Justice', 'Balancing and Just', 'E
 //Code here
 
 
+let maat = new EgyptianGoddesses('Maat', 'Truth and Justice', 'Balancing and Just', 'Exacting in her standards')
 
 //////////////////PROBLEM 2////////////////////
 
@@ -35,9 +28,9 @@ var maatCharacteristics = ['Maat', 'Truth and Justice', 'Balancing and Just', 'E
 
 //BROKEN ON PURPOSE 
 
-function TestScores() {
-  this.name;
-  this.percent;
+function TestScores(name, percent) {
+  this.name = name
+  this.percent = percent
 
   return this.name + this.percent
 }
@@ -47,7 +40,7 @@ function TestScores() {
 
 //Code here
 
-
+const suzy = new TestScores("Suzy", 98)
 
 
 //////////////////PROBLEM 3////////////////////
@@ -59,7 +52,13 @@ function TestScores() {
 
 //Code here
 
-
+function Caveman(name, age, weapon) {
+  this.name = name
+  this.age = age
+  this.weapon = weapon
+  this.health = 100
+  this.energy = 100
+}
 // Now create a new Caveman with the constructor function you made called 'larry'. 
 // Larry is 25 and has a 'rock' for a weapon.
 // Save the result to a variable called larry.
@@ -67,5 +66,5 @@ function TestScores() {
 //Code here
 
 
-
+const larry = new Caveman("Larry", 25, "rock")
 
